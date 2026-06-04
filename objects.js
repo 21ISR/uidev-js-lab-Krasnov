200374f64efb246ede1d890748a24ac30e64643f
Inki-Finki-cat <idiot.mishkin13@gmail.com>
2026-06-04 05:47:00 +0000

    Final version

--- objects.js
+++ objects.js
@@ -14,4 +14,20 @@ const students = [
   { name: "Григорий", grade: 4.1, passed: true },
   { name: "Дарья", grade: 2.5, passed: false },
 ]
-console.log(students.filter(el => el.passed = true).map())
+// console.log(students.filter(el => el.passed).map(el => el.name))
+// console.log(students.find(el => el.grade >= 4.5).name)
+// console.log(students.reduce((acc, el) => acc + el.grade, 0).toFixed(1)/5)
+
+// const u1 = { name: "Иван", age: 20, city: "Казань" }
+// const u2 = { name: "Иван Петров", email: "ivan@mail.ru", city: "Москва" }
+// function mergeUsers(user1, user2){
+//   return {...user1, ...user2}
+// }
+// console.log(mergeUsers(u1, u2))
+
+function countByField(arr, field){
+  const nT = arr.filter(el => el[field]).length
+  const nF = arr.filter(el => el[field] === false).length
+  return `True : ${nT}; False : ${nF}`
+}
+console.log(countByField(students, 'passed'))
